d0874d939a334efdb2ffbc300ee76dcd06aeb382
Michal Terranova <MTerranova@talentpath.com>
2020-12-14 16:55:28 -0500

    update 12-14-20

--- interviewprep/src/main/java/Afternoon121420.java
+++ interviewprep/src/main/java/Afternoon121420.java
@@ -1,8 +1,23 @@
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Afternoon121420 {
     public static void main(String[] args) {
+        List<String> elements = new ArrayList<>();
+        elements.add("a");
+        elements.add("b");
+        elements.add("c");
+        elements.add("d");
+        elements.add("e");
+        elements.add("f");
 
+        List<String> permutations = new ArrayList<>();
+
+        generatePermutations("", elements, permutations);
+        for( String permutation : permutations){
+            System.out.println(permutation);
+        }
     }
 
     //elements is just a collection of substrings(possibly individual characters)
@@ -19,7 +34,12 @@ public class Afternoon121420 {
         if(elements.isEmpty()) {
             perms.add(current);
         } else {
-
+            for(int i=0; i < elements.size(); i++){
+                String element = elements.get(i);
+                elements.remove(i);
+                generatePermutations(current+element, elements, perms );
+                 elements.add(i, element);
+            }
         }
 
     }
