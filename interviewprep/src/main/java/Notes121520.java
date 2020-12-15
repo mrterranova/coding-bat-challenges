ebb0d4068072cc176dfab52fffb8c683c613f553
Michal Terranova <MTerranova@talentpath.com>
2020-12-15 15:02:05 -0500

    update 12-15-20

--- interviewprep/src/main/java/Notes121520.java
+++ interviewprep/src/main/java/Notes121520.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Notes121520 {
     public static void main(String[] args) {
 
@@ -11,7 +13,53 @@ public class Notes121520 {
         //recursively call quicksort on each side of the pivot.
         //main index and a max index passed in.
 
-        
+        //[ v   p    ]
+        //[     p   v]
 
+        //[    p    v]
+        //[    pv    ]
+        //[    vp    ]
+
+        //[v    p    ]
+        //[     vp   ]
+        //[     pv   ]
+
+        //if equal great than
+
+        int pivotIndex = selectPivot(arr, min, max);
+        double pivotValue = arr[pivotIndex];
+
+        for(int i = min; i <= max; i++){
+            if(i == pivotIndex) continue;
+            if(pivotValue < arr[i] && i < pivotIndex){
+                // swap arr[i] to the pivotIndex-1
+                swap(arr, i, pivotIndex-1 );
+                // swap arr[pivotIndex - 1] with arr[pivotIndex]
+                swap(arr, pivotIndex-1, pivotIndex);
+                // i-- //need to stay the same
+                i--;
+                //pivotIndex - 1
+                pivotIndex--;
+            }
+            if(arr[i] < pivotValue && i > pivotIndex){
+                // swap arr[i] to pivotIndex+1
+                swap(arr, i, pivotIndex+1);
+                // swap arr[pivotIndex+1] with arr[pivotIndex]
+                swap(arr, pivotIndex+1, pivotIndex);
+                // pivotIndex + 1
+                pivotIndex++;
+            }
+        }
+        quickSort(arr, min, pivotIndex-1);//left
+        quickSort(arr, pivotIndex+1, max);//right
+    }
+    private static int selectPivot(double[] arr, int start, int end){
+        Random ran = new Random();
+        return (start+end)/2;
+    }
+    private static void swap(double[] arr, int i, int j){
+        double temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
