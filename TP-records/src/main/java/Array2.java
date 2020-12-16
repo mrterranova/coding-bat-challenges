9775f66f44555f2bfdfbeee37bc1675a2df3ccab
Michal Terranova <MTerranova@talentpath.com>
2020-12-16 13:44:55 -0500

    updated 12-16-20

--- TP-records/src/main/java/Array2.java
+++ TP-records/src/main/java/Array2.java
@@ -3,6 +3,8 @@ import java.util.List;
 
 public class Array2 {
     public static void main(String[] args) {
+
+        //COUNT EVENS
         int[] a1 = {1, 3, 5};
         int[] a2 = {2, 2, 0};
         int[] a3 = {1, 3, 5};
@@ -10,6 +12,8 @@ public class Array2 {
         System.out.println(countEvens(a2));//
         System.out.println(countEvens(a3));//
 
+
+        //BIG DIFF
         int[] b1 = {10, 3, 5, 6};
         int[] b2 = {7, 2, 10, 9};
         int[] b3 = {2, 10, 7, 2};
@@ -17,6 +21,7 @@ public class Array2 {
         System.out.println(bigDiff(b2));//
         System.out.println(bigDiff(b3));//
 
+        //CENTERED AVERAGE
         int[] c1 = {1, 2, 3, 4, 100};
         int[] c2 = {1, 1, 5, 5, 10, 8, 7};
         int[] c3 = {-10, -4, -2, -4, -2, 0};
@@ -24,6 +29,7 @@ public class Array2 {
         System.out.println(centeredAverage(c2));//
         System.out.println(centeredAverage(c3));//
 
+        //SUM 13
         int[] d1 = {1, 2, 2, 1};
         int[] d2 = {1, 1};
         int[] d3 = {1, 2, 2, 1, 13};
@@ -31,6 +37,7 @@ public class Array2 {
         System.out.println(sum13(d2));//2
         System.out.println(sum13(d3));//6
 
+        //SUM 67
         int[] e1 = {1, 2, 2};
         int[] e2 = {1, 2, 2, 6, 99, 99, 7};
         int[] e3 ={1, 1, 6, 7, 2};
@@ -38,6 +45,7 @@ public class Array2 {
         System.out.println(sum67(e2));//5
         System.out.println(sum67(e3));//5
 
+        //HAS 22
         int[] f1 = {1, 2, 2};
         int[] f2 = {1, 2, 1, 2};
         int[] f3 = {2, 1, 2};
@@ -45,6 +53,7 @@ public class Array2 {
         System.out.println(has22(f2));//false
         System.out.println(has22(f3));//false
 
+        //LUCKY 13
         int[] g1 = {0, 2, 4};
         int[] g2 = {1, 2, 3};
         int[] g3 = {1, 2, 4};
@@ -52,6 +61,7 @@ public class Array2 {
         System.out.println(lucky13(g2));//
         System.out.println(lucky13(g3));//
 
+        //SUM 28
         int[] h1 = {2, 3, 2, 2, 4, 2};
         int[] h2 = {2, 3, 2, 2, 4, 2, 2};
         int[] h3 = {1, 2, 3, 4};
@@ -59,6 +69,7 @@ public class Array2 {
         System.out.println(sum28(h2));
         System.out.println(sum28(h3));
 
+        //MORE 14
         int[] i1 = {1, 4, 1};
         int[] i2 = {1,4,1,4};
         int[] i3 = {1,1};
@@ -66,14 +77,17 @@ public class Array2 {
         System.out.println(more14(i2));
         System.out.println(more14(i3));
 
+        //FIZZ ARRAY
         System.out.println(fizzArray(4));
         System.out.println(fizzArray(0));
         System.out.println(fizzArray(10));
 
+        //FIZZ ARRAY 2
         System.out.println(fizzArray2(4));
         System.out.println(fizzArray2(10));
         System.out.println(fizzArray2(2));
 
+        //EITHER 24
         int[] j1 = {1,2,2}; //true
         int[] j2 = {4,4,1}; //true
         int[] j3 = {4,4,1,2,2};//false
@@ -81,6 +95,7 @@ public class Array2 {
         System.out.println(either24(j2));
         System.out.println(either24(j3));
 
+        //FIZZ ARRAY 3
         int[] k1 = fizzArray3(5,10); //5,6,7,8,9
         int[] k2 = fizzArray3(11,18); //11,12,13,14,15,16,17
         int[] k3 = fizzArray3(1,3); //1,2
@@ -94,6 +109,8 @@ public class Array2 {
         for(int i =0; i < k3.length; i++){
             System.out.println(k3[i]);
         }
+
+        //PRE 4
         int[]l1 = {1, 2, 4, 1};//1,2
         int[]l2 = {3, 1, 4};//3,1
         int[]l3 = {1,4,4};//1
@@ -110,6 +127,7 @@ public class Array2 {
             System.out.println(l3a[i]);
         }
 
+        //POST 4
         int[]m1 = {2,4,1,2};
         int[]m2 = {4,1,4,2};
         int[]m3 = {4,4,1,2,3};
