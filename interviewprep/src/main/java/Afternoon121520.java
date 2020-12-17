3ce017b56ea86e041bdbe690d4406ddf9a82a680
Michal Terranova <MTerranova@talentpath.com>
2020-12-17 16:29:06 -0500

    updated 12-17-20

--- interviewprep/src/main/java/Afternoon121520.java
+++ interviewprep/src/main/java/Afternoon121520.java
@@ -7,3 +7,33 @@ public class Afternoon121520 {
     //We're told of new assignment in document form that would be issued
     //to each of us for completion.
 }
+
+
+public class SingletonClass{
+    public static void main(String[] args) {
+        ThisIsSingleton object1 = ThisIsSingleton.getInstance();
+    }
+
+}
+class ThisIsSingleton{
+    static ThisIsSingleton thisInstance = new ThisIsSingleton();
+    private ThisIsSingleton(){}
+    public static ThisIsSingleton getInstance(){
+        return thisInstance;
+    }
+}
+
+
+
+class HelloWorld{
+    private String message;
+
+    HelloWorld(){
+        //default
+    }
+
+    HelloWorld(String message){
+        //initializing parameters of the class
+        this.message = message;
+    }
+}
